c41c0c90f3aa20a9e554b0c4334a6149f65ddf4c
Tamir Atzil <tamiratzil93@gmail.com>
2023-09-29 15:43:00 +0300

    modified dad jokes generator to fun facts generator

--- fun_facts_app.py
+++ fun_facts_app.py
@@ -1,10 +1,10 @@
 import openai
 import streamlit as st
 
-def get_dad_joke(topic):
+def get_fun_fact(topic):
     openai.api_key = "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"
 
-    prompt_text = f"Tell me a dad joke about {topic}."
+    prompt_text = f"Give me a fun fact about {topic}."
 
     response = openai.Completion.create(
       model="text-davinci-002",  
@@ -12,12 +12,12 @@ def get_dad_joke(topic):
       max_tokens=150
     )
 
-    joke = response.choices[0].text.strip()
+    fun_fact = response.choices[0].text.strip()
 
-    return joke
+    return fun_fact
 
 # Streamlit UI
-st.set_page_config(page_title="Apple-style Dad Joke Generator", layout="centered")
+st.set_page_config(page_title="Farsi-style Fun Facts Generator", layout="centered")
 
 st.markdown("""
     <style>
@@ -27,17 +27,17 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("Dad Joke Generator 🍎")
+st.title("Fun Facts Generator 🍎")
 st.markdown("""
-For those moments when you need a joke with a specific theme, inspired by the sleek design of Apple.
+For those moments when you are lost in life, and you need some Farsi wisdom to make things better
 """)
 
-topic = st.text_input("🔍 Enter a topic for the dad joke:")
+topic = st.text_input("🔍 Enter a topic for the fun fact:")
 
 if topic:
-    with st.spinner("Generating a sleek joke..."):
-        joke = get_dad_joke(topic)
-    st.success(joke)
+    with st.spinner("Generating a fun fact for you..."):
+        fun_fact = get_fun_fact(topic)
+    st.success(fun_fact)
 
 st.markdown("""
 ---
