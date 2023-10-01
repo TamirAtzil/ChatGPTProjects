aa73bf5e70c05002bf267e7a61805a9cb52d4719
TamirAtzil <47747129+TamirAtzil@users.noreply.github.com>
2023-10-01 15:06:34 +0300

    Update fun_facts_app.py

--- fun_facts_app.py
+++ fun_facts_app.py
@@ -2,15 +2,9 @@ import openai
 import streamlit as st
 
 def get_fun_fact(topic):
-<<<<<<< HEAD
-    openai.api_key = "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"
-
-    prompt_text = f"Give me a fun fact about {topic}."
-=======
-    openai.api_key = "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"
+    openai.api_key = ""
 
     prompt_text = f"Tell me a fun fact about {topic}."
->>>>>>> ea4b659c40d95ede1259481798aab25f65081084
 
     response = openai.Completion.create(
       model="text-davinci-002",
@@ -18,21 +12,12 @@ def get_fun_fact(topic):
       max_tokens=150
     )
 
-<<<<<<< HEAD
-    fun_fact = response.choices[0].text.strip()
-
-    return fun_fact
-
-# Streamlit UI
-st.set_page_config(page_title="Farsi-style Fun Facts Generator", layout="centered")
-=======
     fact = response.choices[0].text.strip()
 
     return fact
 
 # Streamlit UI
 st.set_page_config(page_title="Fun Fact Generator", layout="centered")
->>>>>>> ea4b659c40d95ede1259481798aab25f65081084
 
 st.markdown("""
     <style>
@@ -42,29 +27,17 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-<<<<<<< HEAD
-st.title("Fun Facts Generator 🍎")
-st.markdown("""
-For those moments when you are lost in life, and you need some Farsi wisdom to make things better
-=======
 st.title("Fun Fact Generator 🌟")
 st.markdown("""
 Dive into a world of interesting tidbits on any topic you fancy. Knowledge, after all, is fun!
->>>>>>> ea4b659c40d95ede1259481798aab25f65081084
 """)
 
 topic = st.text_input("🔍 Enter a topic for the fun fact:")
 
 if topic:
-<<<<<<< HEAD
-    with st.spinner("Generating a fun fact for you..."):
-        fun_fact = get_fun_fact(topic)
-    st.success(fun_fact)
-=======
     with st.spinner("Generating an interesting fact..."):
         fact = get_fun_fact(topic)
     st.success(fact)
->>>>>>> ea4b659c40d95ede1259481798aab25f65081084
 
 st.markdown("""
 ---
