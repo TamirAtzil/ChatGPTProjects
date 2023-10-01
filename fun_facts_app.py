264d9ed3c73e5fb74e8f9ac82be1db460265150f
Tamir Atzil <tamiratzil93@gmail.com>
2023-10-01 15:08:29 +0300

    added openai key

--- fun_facts_app.py
+++ fun_facts_app.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 def get_fun_fact(topic):
-    openai.api_key = ""
+    openai.api_key = "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"
 
     prompt_text = f"Tell me a fun fact about {topic}."
 
